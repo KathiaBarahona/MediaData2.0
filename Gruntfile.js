920cd4330243825adf662671ccafbb6f166e4a29
kathybarahona03 <katbara03@gmail.com>
2015-06-21 10:41:06 -0600

    initial commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,20 +8,7 @@
 // 'test/spec/**/*.js'
 
 module.exports = function (grunt) {
-  buildcontrol: {
-    options: {
-        dir: 'dist',
-        commit: true,
-        push: true,
-        message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
-    },
-    heroku: {
-        options: {
-            remote: 'git@heroku.com:heroku-app-mediadataapp.git',
-            branch: 'master'
-        }
-    }
- }
+  var pkg = require('./package.json');
   // Load grunt tasks automatically
   require('load-grunt-tasks')(grunt);
 
@@ -148,22 +135,18 @@ module.exports = function (grunt) {
     },
 
     // Empties folders to start fresh
-   clean: {
-    dist: {
+    clean: {
+      dist: {
         files: [{
-            dot: true,
-            src: [
-                '.tmp',
-                '<%= yeoman.dist %>/*',
-                '!<%= yeoman.dist %>/.git{,*/}*',
-                '!<%= yeoman.dist %>/Procfile',
-                '!<%= yeoman.dist %>/package.json',
-                '!<%= yeoman.dist %>/web.js',
-                '!<%= yeoman.dist %>/node_modules'
-           ]
+          dot: true,
+          src: [
+            '.tmp',
+            '<%= yeoman.dist %>/{,*/}*',
+            '!<%= yeoman.dist %>/.git{,*/}*'
+          ]
         }]
-    },
-    server: '.tmp'
+      },
+      server: '.tmp'
     },
 
     // Add vendor prefixed styles
@@ -396,7 +379,33 @@ module.exports = function (grunt) {
         'svgmin'
       ]
     },
-
+    buildcontrol: {
+        options: {
+          dir: 'dist',
+          commit: true,
+          push: true,
+          message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
+        },
+        pages: {
+          options: {
+            remote: 'git@github.com:kathybarahona03/MediaData2.0.git',
+            branch: 'gh-pages'
+          }
+        },
+        heroku: {
+          options: {
+            remote: 'git@heroku.com:example-heroku-webapp-1988.git',
+            branch: 'master',
+            tag: pkg.version
+          }
+        },
+        local: {
+          options: {
+            remote: '../',
+            branch: 'build'
+          }
+        }
+    },
     // Test settings
     karma: {
       unit: {
@@ -405,7 +414,7 @@ module.exports = function (grunt) {
       }
     }
   });
-  grunt.registerTask('deploy', ['buildcontrol']);
+
 
   grunt.registerTask('serve', 'Compile then start a connect web server', function (target) {
     if (target === 'dist') {
